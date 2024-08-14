38a5c9a549ed81b46b224afe6844ee2049a2f031
oskarmozdzen <oskar.mozdzen@daw-systems.pl>
2024-08-14 15:56:02 +0200

    added functional longs and shorts, will have to check for bugs, check if adjustments in finding the consolidation are needed

--- main_optim.py
+++ main_optim.py
@@ -122,14 +122,95 @@ def overlaps(list1, list2):
     return bool(len(common_items))
 
 
-def check_trade():
-    ...
+class Trade:
+    def __init__(self, entry_price, stop_loss, take_profit, date, size=100):
+        self.size = size
+        self.entry_price = entry_price
+        self.stop_loss = stop_loss
+        self.take_profit = take_profit
+        self.type = "LONG" if stop_loss < entry_price else "SHORT"
+        # print(f"{self.type} TRADE_OPENED AT {entry_price} ON {date}")
+        self.open_date = date
+        self.percentage_loss = 1 - abs(entry_price - stop_loss) / entry_price
+        self.percentage_profit = 1 + abs(entry_price - take_profit) / entry_price
+        self.is_open = True
+        self.is_in_game = False
+        self.is_profit = None
 
-# DECIDE WHAT IS THE ENTRY POINT
-def open_trade(entry):
-    ...
+    def check_trade(self, current_candle):
+        # if self.type == "LONG" and self.open_date.minute == 32 and self.open_date.second == 15:
+        #     print(current_candle['Low'], self.stop_loss, current_candle['High'])
 
+        if self.is_in_game:
+            if type == "SHORT":
+                if current_candle['High'] >= self.stop_loss:
+                    self.is_open = False
+                    self.is_profit = False
+                    self.trade_return = self.percentage_loss * self.size
+                    print(f"{self.type} {self.open_date} TRADE CLOSED ON LOSS: {self.trade_return:.2f}")
+                    return self.is_profit, self.trade_return
+                elif current_candle['Low'] <= self.take_profit:
+                    self.is_open = False
+                    self.is_profit = True
+                    self.trade_return = self.percentage_profit * self.size
 
+                    print(f"{self.type} {self.open_date} TRADE CLOSED ON PROFIT: {self.trade_return:.2f}")
+                    return self.is_profit, self.trade_return
+            else:
+                if current_candle['Low'] <= self.stop_loss:
+                    self.is_open = False
+                    self.is_profit = False
+                    self.trade_return = self.percentage_loss * self.size
+                    print(f"{self.type} {self.open_date} TRADE CLOSED ON LOSS: {self.trade_return:.2f}")
+                    return self.is_profit, self.trade_return
+                elif current_candle['High'] >= self.take_profit:
+                    self.is_open = False
+                    self.is_profit = True
+                    self.trade_return = self.percentage_profit * self.size
+                    print(f"{self.type} {self.open_date} TRADE CLOSED ON PROFIT: {self.trade_return:.2f}")
+                    return self.is_profit, self.trade_return
+        else:
+            if current_candle['Low'] <= self.entry_price <= current_candle['High']:
+                self.is_in_game = True
+                print(f"{self.type} {self.open_date} {self.entry_price:.2f} TRADE IS IN GAME")
+        return None, None
+
+    def cancel_trade(self):
+        self.is_open = False
+        self.is_in_game = False
+
+class Trader:
+    def __init__(self):
+        self.trades = []
+        self.trades_history = []
+        self.total_money = 0
+        self.trades_closed = 0
+
+    def show_stats(self):
+        avg_return = 0 if self.trades_closed == 0 else self.total_money / self.trades_closed
+        print(f"AVG RETURN PER TRADE: {avg_return:.2f}, TOTAL: {(self.total_money - (100 * self.trades_closed)):.2f}")
+
+    def check_trade(self, current_candle):
+        change = False
+        for trade in self.trades:
+            if trade.is_open:
+                status, trade_return = trade.check_trade(current_candle)
+                if status is not None:
+                    self.total_money += trade_return
+                    self.trades_closed += 1
+                    change = True
+        return change
+
+    def cancel_unopened_trades(self):
+        for trade in self.trades:
+            trade.cancel_trade()
+
+    # DECIDE WHAT IS THE ENTRY POINT
+    def open_trade(self, entry_price, stop_loss, take_profit, date):
+        new_trade = Trade(entry_price, stop_loss, take_profit, date)
+        self.trades.append(new_trade)
+
+trader = Trader()
 
 df = pd.read_csv('BTCUSDT-1s-2024-08-07.csv')
 df.columns = ["Date", "Open", "High", "Low", "Close", "Volume", "close_time", "quote_volume", "count",
@@ -137,7 +218,7 @@ df.columns = ["Date", "Open", "High", "Low", "Close", "Volume", "close_time", "q
 df = df[["Date", "Open", "High", "Low", "Close", "Volume"]]
 
 df['Date'] = pd.to_datetime(df['Date'], unit='ms')
-df = df[(df['Date'] > '2024-08-07 14:30:10') & (df['Date'] < '2024-08-07 14:40:41')]
+df = df[(df['Date'] > '2024-08-07 00:00:01') & (df['Date'] < '2024-08-07 23:59:59')]
 # mpf.plot(df.set_index('Date'), type='candle', style='charles', title='BTC Candlestick Chart', ylabel='Price',
 #          datetime_format='%H:%M:%S')
 
@@ -148,14 +229,19 @@ df.reset_index(inplace=True)
 
 previous_consolidation_peak = None
 for idx in range(max_window_width-1, len(df), 1):
-    start_date = df.loc[idx]['Date'].to_pydatetime()
+    current_candle = df.loc[idx]
+    start_date = current_candle['Date'].to_pydatetime()
     # print(start_date)
     t = time.time()
     found_in_window = False
 
+    show = False
     max_window_size = 0
 
-    check_trade()
+    is_change = trader.check_trade(current_candle)
+    if is_change:
+        show = True
+        trader.show_stats()
 
     for i in range(min_window_width, max_window_width, 1):
         candles_range = df.loc[idx - i:idx].copy()
@@ -181,24 +267,29 @@ for idx in range(max_window_width-1, len(df), 1):
         #CHECK IF FOUND CONSECUTIVE CONSOLIDATIONS, IF ARE CONSECUTIVE CHECK IF THE PEAK IS TOP OR BOTTOM, IF IS THE SAME AS PREVIOUS THEN DO NOTHING ELSE OPEN A TRADE
 
         if found:
-            show = True
             last_peak = grouped_peaks.iloc[-1]
             if previous_consolidation_peak is None:
                 ...
-                print(last_peak['Half'], previous_consolidation_peak)
+                # print(last_peak['Half'], previous_consolidation_peak)
                 show = True
                 # DO A TRADE
+                if last_peak['Half']:
+                    trader.open_trade(entry_price=mean_bottom, stop_loss=bottom_border_min, take_profit=top_border_min, date=start_date)
+                    # IF WE ARE ON TOP THEN TRY TO FIND A LONG FROM BOTTOM
+                else:
+                    trader.open_trade(entry_price=mean_top, stop_loss=top_border_max, take_profit=bottom_border_max, date=start_date)
+                    # IF WE ARE ON BOTTOM THEN TRY TO FIND A SHORT FROM TOP
             else:
-                print(last_peak['Half'], previous_consolidation_peak['Half'])
+                # print(last_peak['Half'], previous_consolidation_peak['Half'])
                 if previous_consolidation_peak['Half'] != last_peak['Half']:
                     show = True
                     if last_peak['Half']:
-                        open_trade(mean_bottom)
+                        trader.open_trade(entry_price=mean_bottom, stop_loss=bottom_border_min, take_profit=top_border_min, date=start_date)
                         # IF WE ARE ON TOP THEN TRY TO FIND A LONG FROM BOTTOM
                     else:
-                        open_trade(mean_top)
+                        trader.open_trade(entry_price=mean_top, stop_loss=top_border_max, take_profit=bottom_border_max, date=start_date)
                         # IF WE ARE ON BOTTOM THEN TRY TO FIND A SHORT FROM TOP
-            if show:
+            if show and False:
                 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 10))
 
                 mpf.plot(candles_range[::-1].set_index('Date'), type='candle', style='charles', ylabel='Price',
@@ -219,13 +310,16 @@ for idx in range(max_window_width-1, len(df), 1):
                 # plt.savefig(f'consolidations_new/{str(candles_range["Date"].values[0]).replace(":", "-").replace(" ", "_")}-{str(candles_range["Date"].values[-1]).replace(":", "-").replace(" ", "_")}.png')
                 plt.show()
 
-                plt.cla()
-                plt.clf()
+                # plt.cla()
+                # plt.clf()
             previous_consolidation_peak = last_peak
 
-            print(previous_consolidation_peak)
+            # print(previous_consolidation_peak)
         else:
             previous_consolidation_peak = None
+    else:
+        trader.cancel_unopened_trades()
+
     # if found_in_window == False and len(previous_consolidation_dates) != 0:
     #     print(previous_consolidation_dates[0], previous_consolidation_dates[-1])
     #
@@ -258,6 +352,6 @@ for idx in range(max_window_width-1, len(df), 1):
     #         # with open("results.txt", 'w+') as f:
     #         #     f.write(f"{str(start_date)}\n")
     #     previous_consolidation_dates = []
-    print(time.time() - t)
+    # print(time.time() - t)
 
 
